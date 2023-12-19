58eb2664bdda7537738bfcfce3916dae6fc85b5d
Dan Whitaker <dan@uncharted.systems>
2023-12-18 20:44:23 -0700

    added readme with build link

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zig Learning
+[![Build Status](https://github.com/UnchartedSystems/Zig-Learning/actions/workflows/CI.yml/badge.svg)](https://github.com/UnchartedSystems/Zig-Learning/actions)
--- build.zig
+++ build.zig
@@ -10,13 +10,7 @@ pub fn build(b: *Builder) void {
         .target = target,
         .optimize = optimize,
     });
-    if (target.isNativeOs() and target.getOsTag() == .linux) {
-        // The SDL package doesn't work for Linux yet, so we rely on system
-        // packages for now.
-        //exe.linkSystemLibrary("SDL2");
-        exe.linkLibC();
-    }
-    
+
     b.installArtifact(exe);
 
     const run = b.step("run", "Run the demo");
